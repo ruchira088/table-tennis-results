899adc9d6c8521efeca574dd4f76515cf757f476
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2017-09-03 23:42:33 +1000

    Cloning the git repos using SSH rather than HTTPS

--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 GIT_REPOS=(
-	"https://github.com/ruchira088/table-tennis-team-service.git"
+	"git@github.com:ruchira088/table-tennis-team-service.git"
+    "git@github.com:ruchira088/table-tennis-results-service.git"
+	"git@github.com:ruchira088/table-tennis-web.git"
 )
 
 for gitRepo in "${GIT_REPOS}"
