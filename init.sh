4570d72a17f9d4fa577f646f7e2c8d5094e847f2
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2017-09-04 18:38:00 +1000

    Fixing the init.sh file

--- init.sh
+++ init.sh
@@ -2,11 +2,11 @@
 
 GIT_REPOS=(
 	"git@github.com:ruchira088/table-tennis-team-service.git"
-    "git@github.com:ruchira088/table-tennis-results-service.git"
+    	"git@github.com:ruchira088/table-tennis-results-service.git"
 	"git@github.com:ruchira088/table-tennis-web.git"
 )
 
-for gitRepo in "${GIT_REPOS}"
+for gitRepo in "${GIT_REPOS[@]}"
 do
 	git clone $gitRepo
 done
